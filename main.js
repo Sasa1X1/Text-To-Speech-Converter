568a198e179cfbfdfae7ef8dab15a0d251f0cbd0
sasa? <147984307+Sasa1X1@users.noreply.github.com>
2024-03-22 12:07:24 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
 let speech = new SpeechSynthesisUtterance();
-
 let voices = [];
+let voiceselect = document.querySelector('select');
 
-let voiceselect  = document.querySelector('select')
+// Function to populate voices array with all available voices
+function populateVoices() {
+  voices = window.speechSynthesis.getVoices();
+  voiceselect.innerHTML = ""; // Clear existing options
+  voices.forEach((voice, i) => (voiceselect.options[i] = new Option(voice.name, i)));
+}
 
-window.speechSynthesis.onvoiceschanged = () => {
-    voices = window.speechSynthesis.getVoices();
-    speech.voice = voices[0];
+// Populate voices array initially and when voiceschanged event is fired
+populateVoices();
+window.speechSynthesis.onvoiceschanged = populateVoices;
 
-    voices.forEach((voice, i) => (voiceselect.options[i] = new Option(voice.name, i)))
-};
-voiceselect.addEventListener("change", () =>{
-    speech.voice = voices[voiceselect.value];
+// Event listener for changing voice
+voiceselect.addEventListener("change", () => {
+  speech.voice = voices[voiceselect.value];
 });
 
-    document.querySelector("button").addEventListener('click', () =>{
-        speech.text = document.querySelector("textarea").value;
-        window.speechSynthesis.speak(speech);
+// Event listener for button click or touch to speak text
+document.querySelector("button").addEventListener('click', speakText);
+document.querySelector("button").addEventListener('touchstart', speakText);
 
-});
+function speakText() {
+  speech.text = document.querySelector("textarea").value;
+  window.speechSynthesis.speak(speech);
+}
